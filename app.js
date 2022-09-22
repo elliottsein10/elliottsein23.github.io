7c1b6265c611200d223645a86519744341f9b825
Elliott Sánchez León <raptordroid15@gmail.com>
2022-09-22 12:18:59 -0500

    Commit MagicBall

---
+++ app.js
@@ -0,0 +1,46 @@
+const ask = () => {
+    //Leer Pregunta
+    const questionInput = document.getElementById("question");
+    const answerInput = document.getElementById('answer');
+    if (questionInput.value !== ""){
+        //Recibir respuesta al preguntar a bola mágina
+        let answer = magicBall();
+        //Imprimir Resultado
+        answerInput.value = answer;
+    } else {
+        answerInput.value = "Pregunta algo!!";
+    }
+};
+
+//Función Bola Mágica
+const magicBall = () => {
+    let randomNumber = Math.floor(Math.random() * 8);
+    let eightBall = '';
+    switch (randomNumber){
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+    }
+    return eightBall;
+};
